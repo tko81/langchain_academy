7d8ce10c3741ce769479c03fce06cbfc3eada8c6
Wang, Xin5 <xin5.wang@intel.com>
2025-08-18 11:12:14 +0800

    update

--- module-4/studio/parallelization.py
+++ module-4/studio/parallelization.py
@@ -8,11 +8,11 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_community.tools import TavilySearchResults
 
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models.tongyi import ChatTongyi
 
 from langgraph.graph import StateGraph, START, END
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0) 
+llm = ChatTongyi(model="gpt-4o", temperature=0) 
 
 class State(TypedDict):
     question: str
--- module-4/studio/research_assistant.py
+++ module-4/studio/research_assistant.py
@@ -6,14 +6,14 @@ from typing_extensions import TypedDict
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, get_buffer_string
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models.tongyi import ChatTongyi
 
 from langgraph.constants import Send
 from langgraph.graph import END, MessagesState, START, StateGraph
 
 ### LLM
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0) 
+llm = ChatTongyi(model="gpt-4o", temperature=0) 
 
 ### Schema 
 
